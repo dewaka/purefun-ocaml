82e5d57318c1f13e7317f63889bad663dafd6b90
Chathura Colombage <dcdewaka@gmail.com>
2014-06-06 23:16:25 +0100

    Adding auxiliary functions

--- list.ml
+++ list.ml
@@ -16,4 +16,29 @@ module List = struct
   let rec reverse = function
     | Nil -> Nil
     | Cons (x, xs') -> append (reverse xs') (Cons (x, Nil))
+
+  let rec map f = function
+    | Nil -> Nil
+    | Cons (x, xs') -> Cons (f x, map f xs')
+
+  let rec foldl f a xs =
+    match xs with
+    | Nil -> a
+    | Cons (x, xs') -> foldl f (f a x) xs'
+
 end
+
+let sample_x = Cons (1, Cons (2, Nil))               
+
+let sum xs =
+  foldl (fun x y -> x + y) 0 xs
+
+let product xs =
+  foldl (fun x y -> x * y) 1 xs
+        
+let rec range ?(i=1) m n =
+  match m, n with
+  | m, n when m <= n -> Cons (m, range ~i:i (m+i) n)
+  | _, _ -> Nil
+  
+let sample_y = range 1 10 1;;
