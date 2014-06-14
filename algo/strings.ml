2ef0874aeb3f7276057b572bd7d4cb8989e8e283
Chathura Colombage <dcdewaka@gmail.com>
2014-06-14 01:14:27 +0100

    Brute force string search algorithm

--- algo/strings.ml
+++ algo/strings.ml
@@ -21,3 +21,15 @@ let explode str =
 let starts_with s str = is_prefix (explode s) (explode str)
 
 let substring s str = sub_list (explode s) (explode str)
+
+let simple_search s str =
+  let m, n = String.length s, String.length str in
+  let rec prefix i j =
+    if i >= m then true
+    else if j >= n then false
+    else (String.get s i) = (String.get str j) && prefix (i+1) (j+1)
+  in
+  let rec search j =
+    if j >= n then false
+    else prefix 0 j || search (j+1)
+  in search 0
