1beabce3b5429bc8b796688c0f95cb239bf72f9e
Chathura Colombage <dcdewaka@gmail.com>
2014-06-18 15:38:30 +0100

    Fixed binary_search

--- algo/searching.ml
+++ algo/searching.ml
@@ -1,14 +1,13 @@
 let binary_search x arr =
-	let n = Array.length arr in
-	let rec find i j =
-		if i = j then false
-		else
-			let m = (i + j) / 2 in
-			let mid = Array.get arr m in
-			if mid=x then true
-			else 
-				if x<mid then find i m
-				else find m j
-	in
-	find 0 n
-
+  let n = Array.length arr - 1 in
+  let rec find i j =
+    if i > j then None
+    else
+      let m = (i + j) / 2 in
+      let mid = Array.get arr m in
+      if mid=x then Some(m)
+      else
+        if x<mid then find i (m-1)
+        else find (m+1) j
+  in
+  find 0 n
