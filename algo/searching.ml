9774e417ce078dc436da58a575f08887a745b35d
Chathura Colombage <dcdewaka@gmail.com>
2014-06-19 20:47:51 +0100

    Binary assoc array search

--- algo/searching.ml
+++ algo/searching.ml
@@ -11,3 +11,17 @@ let binary_search x arr =
         else find (m+1) j
   in
   find 0 n
+
+let binary_assoc_search k arr =
+  let n = Array.length arr - 1 in
+  let rec find i j =
+    if i > j then None
+    else
+      let m = (i + j) / 2 in
+      let (mid, v) = Array.get arr m in
+      if mid=k then Some(v)
+      else
+        if k<mid then find i (m-1)
+        else find (m+1) j
+  in
+  find 0 n
